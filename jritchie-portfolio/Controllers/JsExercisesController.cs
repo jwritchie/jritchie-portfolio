034bb22c154c494ea3ec276d2944c12b80f9e251
jwritchie <JRitchie.NC@gmail.com>
2017-09-18 11:00:58 -0400

    Initial commit

---
+++ jritchie-portfolio/Controllers/JsExercisesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace jritchie_portfolio.Controllers
+{
+    public class JsExercisesController : Controller
+    {
+        // GET: JsExercises
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ jritchie-portfolio/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="jritchie_portfolio.MvcApplication" Language="C#" %>
